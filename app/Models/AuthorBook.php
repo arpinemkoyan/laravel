b4a4103cc2a098a351c7de69860a578ed16b2afe
sahartak2025 <sahartak2025@gmail.com>
2022-12-06 11:48:31 +0400

    remove authorBook required

--- app/Models/AuthorBook.php
+++ app/Models/AuthorBook.php
@@ -9,7 +9,5 @@ class AuthorBook extends Model
 {
     use HasFactory;
     public $table = "author_book";
-    protected $fillable = [
-        'book_id', 'author_id'
-    ];
+
 }
--- app/Services/AuthorBooksService.php
+++ app/Services/AuthorBooksService.php
@@ -10,15 +10,20 @@ class AuthorBooksService
 {
     public function createAuthorBook($bookId, $authorId)
     {
-        return AuthorBook::create([
-            'book_id' => $bookId,
-            'author_id' => $authorId
-        ]);
+        $authorBook = new AuthorBook();
+        $authorBook->book_id=$bookId;
+        $authorBook->author_id=$authorId;
+//        return AuthorBook::create([
+//            'book_id' => $bookId,
+//            'author_id' => $authorId
+//        ]);
+        $authorBook->save();
+        return;
     }
 
     public function updateAuthorBook($bookId, $authorsId)
     {
-        $data = AuthorBook::where('book_id', '=', $bookId);
+        $data = AuthorBook::where(['book_id' => $bookId]);
         $data->delete();
         foreach ($authorsId as $author_id) {
             $data->insert([
